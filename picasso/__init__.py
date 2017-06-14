a2337b682521af684919a952c818937a2d7b8555
ryan <hendersonry@gmail.com>
2017-06-14 12:33:27 +0200

    add deprecation error

--- picasso/__init__.py
+++ picasso/__init__.py
@@ -18,4 +18,18 @@ app.config.from_object('picasso.settings.Default')
 if os.getenv('PICASSO_SETTINGS'):
     app.config.from_envvar('PICASSO_SETTINGS')
 
+deprecated_settings = ['BACKEND_PREPROCESSOR_NAME',
+                       'BACKEND_PREPROCESSOR_PATH',
+                       'BACKEND_POSTPROCESSOR_NAME',
+                       'BACKEND_POSTPROCESSOR_PATH',
+                       'BACKEND_PROB_DECODER_NAME',
+                       'BACKEND_PROB_DECODER_PATH']
+
+if any([x in app.config.keys() for x in deprecated_settings]):
+    raise ValueError('It looks like you\'re using a deprecated'
+                     ' setting.  The settings and utility functions'
+                     ' have been changed as of version v0.2.0 (and '
+                     'you\'re using {}). Changing to the updated '
+                     ' settings is trivial: see xxxxxxx.'.format(__version__))
+
 import picasso.picasso
