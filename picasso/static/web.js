74e5cb8fa56752b87398088df665141ee87fba3b
Jan Steinke <jan.steinke@gmail.com>
2017-07-29 22:12:54 +0200

    only update the list once all elements are collected

--- picasso/static/web.js
+++ picasso/static/web.js
@@ -2,17 +2,21 @@ $("document").ready(function() {
 
     var vis_select = $('#vis-select');
     var settings_list = $('#settings-list');
+    var visItems = "";
+
     $.ajax({
         type: 'GET',
         url: '/api/visualizers',
         success: function(data) {
             $.each(data.visualizers, function(i, visualization) {
-                vis_select.append('<option>'+ visualization.name +'</option>');
+                visItems += '<option>'+ visualization.name +'</option>';
             });
+            vis_select.append(visItems)
         }
     });
 
     $('#vis-select').on('change', function() {
+        var optionItems = "";
         var selected_vis = this.options[this.selectedIndex].text;
         settings_list.empty();
         $.ajax({
@@ -20,8 +24,9 @@ $("document").ready(function() {
             url: '/api/visualizers/' + selected_vis,
             success: function(data) {
                 $.each(data.settings, function(setting, options) {
-                    settings_list.append('<li>' + setting + '<select class="form-control" id='+setting+'-setting></select></li>')
+                    optionItems += '<li>' + setting + '<select class="form-control" id=' + setting + '-setting></select></li>';
                 });
+                settings_list.append(optionItems);
             }
         });
     });
