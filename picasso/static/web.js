320ebdb79384526bf71a4b06eb16204b6c5a9742
Jan Steinke <jan.steinke@gmail.com>
2017-07-29 22:39:35 +0200

    refactor AJAX calls into functions for better readability

--- picasso/static/web.js
+++ picasso/static/web.js
@@ -4,31 +4,39 @@ $("document").ready(function() {
     var settings_list = $('#settings-list');
     var visItems = "";
 
-    $.ajax({
-        type: 'GET',
-        url: '/api/visualizers',
-        success: function(data) {
-            $.each(data.visualizers, function(i, visualization) {
-                visItems += '<option>'+ visualization.name +'</option>';
-            });
-            vis_select.append(visItems)
-            vis_select.change()
-        }
+    function getVisualizers() {
+        return $.ajax({
+            type: 'GET',
+            url: '/api/visualizers',
+        });
+    }
+
+    function getVisualizerSettings(visualizer) {
+        return $.ajax({
+            type: 'GET',
+            url: '/api/visualizers/' + visualizer,
+        });
+    }
+
+    getVisualizers().done(function(data) {
+        $.each(data.visualizers, function(i, visualization) {
+            visItems += '<option>'+ visualization.name +'</option>';
+        });
+        vis_select.append(visItems)
+        vis_select.change()
     });
+
 
     vis_select.on('change', function() {
         var optionItems = "";
         var selected_vis = this.options[this.selectedIndex].text;
         settings_list.empty();
-        $.ajax({
-            type: 'GET',
-            url: '/api/visualizers/' + selected_vis,
-            success: function(data) {
-                $.each(data.settings, function(setting, options) {
-                    optionItems += '<li>' + setting + '<select class="form-control" id=' + setting + '-setting></select></li>';
-                });
-                settings_list.append(optionItems);
-            }
+        getVisualizerSettings(selected_vis).done(function(data) {
+            $.each(data.settings, function(setting, options) {
+                optionItems += '<li>' + setting + '<select class="form-control" id=' + setting + '-setting></select></li>';
+            });
+            settings_list.append(optionItems);
+
         });
     });
 });
