5bd2aa43048572907cdc1ae5de3de18052ce6eb2
Jan Steinke <jan.steinke@gmail.com>
2017-07-29 21:53:31 +0200

    follow a few best practices

--- picasso/static/web.js
+++ picasso/static/web.js
@@ -1,26 +1,26 @@
-$(function() {
+$("document").ready(function() {
 
-    var $vis_select = $('#vis-select');
-    var $settings_list = $('#settings-list');
+    var vis_select = $('#vis-select');
+    var settings_list = $('#settings-list');
     $.ajax({
         type: 'GET',
         url: '/api/visualizers',
         success: function(data) {
             $.each(data.visualizers, function(i, visualization) {
-                $vis_select.append('<option>'+ visualization.name +'</option>');
+                vis_select.append('<option>'+ visualization.name +'</option>');
             });
         }
     });
 
     $('#vis-select').on('change', function() {
-        var $selected_vis = this.options[this.selectedIndex].text;
-        $settings_list.empty();
+        var selected_vis = this.options[this.selectedIndex].text;
+        settings_list.empty();
         $.ajax({
             type: 'GET',
-            url: '/api/visualizers/' + $selected_vis,
+            url: '/api/visualizers/' + selected_vis,
             success: function(data) {
-                $.each(data.settings, function(i, setting) {
-                    $settings_list.append('<li><select class="form-control" name='+ setting +'></select></li>')
+                $.each(data.settings, function(setting, options) {
+                    settings_list.append('<li>' + setting + '<select class="form-control" id='+setting+'-setting></select></li>')
                 });
             }
         });
