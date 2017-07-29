80bcaef72ee2cd03674d320149ad8bbead115d0e
Jan Steinke <jan.steinke@gmail.com>
2017-07-29 16:18:37 +0200

    dynamically add and remove vis settings boxes
     This isn't fully working yet. I still have to add the actual settings to it and make it work on the initial loading of the page. Currently you have to at least select something in the box before getting boxes for the options.

--- picasso/static/web.js
+++ picasso/static/web.js
@@ -1,6 +1,7 @@
 $(function() {
 
     var $vis_select = $('#vis-select');
+    var $settings_list = $('#settings-list');
     $.ajax({
         type: 'GET',
         url: '/api/visualizers',
@@ -9,5 +10,19 @@ $(function() {
                 $vis_select.append('<option>'+ visualization.name +'</option>');
             });
         }
+    });
+
+    $('#vis-select').on('change', function() {
+        var $selected_vis = this.options[this.selectedIndex].text;
+        $settings_list.empty();
+        $.ajax({
+            type: 'GET',
+            url: '/api/visualizers/' + $selected_vis,
+            success: function(data) {
+                $.each(data.settings, function(i, setting) {
+                    $settings_list.append('<li><select class="form-control" name='+ setting +'></select></li>')
+                });
+            }
+        });
     });
 });
