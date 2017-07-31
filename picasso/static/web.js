d1a5979b8332616df134187dd4f8d74ec5b016fa
Jan Steinke <jan.steinke@gmail.com>
2017-07-31 15:13:27 +0200

    implement loading the setting options into the respective fields
     The UI loads now all necessary elements to properly send a visualize request.

--- picasso/static/web.js
+++ picasso/static/web.js
@@ -17,6 +17,24 @@ $("document").ready(function() {
         });
     }
 
+    function loadVisualizerSettings(visualizerSettings) {
+        var settingItems;
+        $.each(visualizerSettings, function(setting, options) {
+            settingItems += setting + '<select class="form-control" id=' + setting + '-setting></select>';
+        });
+        settings_list.append(settingItems);
+    }
+
+    function loadVisualizerOptions(visualizerSettings) {
+        $.each(visualizerSettings, function(setting, options) {
+            var optionItems;
+            $.each(options, function(i, option) {
+                optionItems += '<option>'+ option + '</option>';
+            })
+            $('#'+setting+'-setting').append(optionItems);
+        })
+    }
+
     getVisualizers().done(function(data) {
         var visItems = "";
         $.each(data.visualizers, function(i, visualization) {
@@ -26,17 +44,14 @@ $("document").ready(function() {
         vis_select.change()
     });
 
-
     vis_select.on('change', function() {
-        var optionItems = "";
         var selected_vis = this.options[this.selectedIndex].text;
+        var visualizerSettings;
         settings_list.empty();
         getVisualizerSettings(selected_vis).done(function(data) {
-            $.each(data.settings, function(setting, options) {
-                optionItems += setting + '<select class="form-control" id=' + setting + '-setting></select>';
-            });
-            settings_list.append(optionItems);
-
+            selected_vis_settings = data.settings;
+            loadVisualizerSettings(selected_vis_settings);
+            loadVisualizerOptions(selected_vis_settings);
         });
     });
 });
