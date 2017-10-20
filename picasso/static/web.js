34566dc366c13647fdd011f09899620c892575a4
Jan Steinke <jan.steinke@gmail.com>
2017-10-20 23:32:15 +0200

    add license headers

--- picasso/static/web.js
+++ picasso/static/web.js
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2017 Merantix GmbH
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jan Steinke - Restful API
+ */
 $("document").ready(function() {
 
     var select_vis = $('#select_vis');
